90de1239aaff166f8382f5232769b24bdbb5b851
jiulingxiaoxin <jiulingxiaoxin@163.com>
2020-06-24 19:21:10 +0800

    新增 vue2-ts,vue2-js,webpack-ts的命令行参数

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,4 +1,5 @@
 const program = require('commander');
+const error = require('chalk').bold.red;
 
 program
     //cli版本号
@@ -6,13 +7,31 @@ program
 
 program
     //初始化项目
-    .command('init <name>')
+    .command('init <name> [otherDirs...]')
     .description('generate a new project from a template')
-    .action((name) => {
+    .action((name, otherDirs) => {
             // if (!name) {
             //     console.log('Every great project needs a name!');
             // }
-            require('./init')('direct:' + require('../package').template.h5, process.cwd() + '\\' + name, {clone: false});
+            // 查看是否有除init外的剩余参数
+            if (otherDirs.length) {
+              switch (otherDirs[0]) {
+                case 'vue2-ts':
+                  require('./init')('direct:' + require('../package').template['vue2-starter-ts'], process.cwd() + '\\' + name, {clone: false});
+                  break;
+                case 'vue2-js':
+                  require('./init')('direct:' + require('../package').template['vue2-starter-js'], process.cwd() + '\\' + name, {clone: false});
+                  break;
+                case 'webpack-ts':
+                  require('./init')('direct:' + require('../package').template['typescript-webpack-starter'], process.cwd() + '\\' + name, {clone: false});
+                  break;
+                default: 
+                  otherDirs.reduce((val1, val2) => (val1 + val2));
+                  console.log(error(`error: ${otherDirs} doesn't exist.`));
+              }
+            } else {
+              require('./init')('direct:' + require('../package').template.h5, process.cwd() + '\\' + name, {clone: false});
+            }
         }
     );
 
